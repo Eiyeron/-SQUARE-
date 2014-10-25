514e9b469b3346f3e0248bc410ae8895f5e9bf87
Eiyeron Fulmincendii <eiyeron@gmail.com>
2014-10-25 09:47:53 +0200

    Started private var convention changes.

--- src/GameState.hx
+++ src/GameState.hx
@@ -19,80 +19,80 @@ import components.RotatingEntity;
 
 typedef GameStateTypedArgs = {
 	name : String,
-	cube : PlayerSquare
+	square : PlayerSquare
 }
 
 class GameState extends luxe.State {
 
 	public static inline var highscoreKey:String =" [SQUARE] highscore";
 
-	private var cube     : PlayerSquare;
-	private var started  : Bool;
-	private var obstacles: Array<Obstacle>;
-	private var bonuses  : Array<Pickup>;
-	private var ev       : Events;
-	private var score    : Float;
-	private var score_txt: MenuText;
-	private var font     : BitmapFont;
-	private var volume   : Float;
-	private var eManager : EffectManager;
+	private var _square    : PlayerSquare;
+	private var _started   : Bool;
+	private var _obstacles : Array<Obstacle>;
+	private var _bonuses    : Array<Pickup>;
+	private var _ev         : Events;
+	private var _score      : Float;
+	private var _score_txt  : MenuText;
+	private var _font       : BitmapFont;
+	private var _volume     : Float;
+	private var _eManager   : EffectManager;
 
 	public function new( data:GameStateTypedArgs ) {
 		super({ name:data.name });
-		cube = data.cube;
-		obstacles = new Array<Obstacle>();
-		bonuses = new Array<Pickup>();
-		ev = new Events();
-		font = Luxe.resources.find_font('open_sans');
-		score_txt = new MenuText(new Vector(Luxe.screen.mid.x, Luxe.screen.mid.y + 50),
-			"0", font, 96, 0xD64937);
-		score_txt.color.a = 0;
-		score_txt.depth = -0.5;
-		eManager = new EffectManager();
+		_square = data.square;
+		_obstacles = new Array<Obstacle>();
+		_bonuses = new Array<Pickup>();
+		_ev = new Events();
+		_font = Luxe.resources.find__font('open_sans');
+		_score_txt = new MenuText(new Vector(Luxe.screen.mid.x, Luxe.screen.mid.y + 50),
+			"0", _font, 96, 0xD64937);
+		_score_txt.color.a = 0;
+		_score_txt.depth = -0.5;
+		_eManager = new EffectManager();
 
 	}
 
 	override function init<T>() {
-		started = false;
-		ev.listen("addPickup", addPickup);
-		ev.listen("addObstacle", addObstacle);
-		ev.listen("gameOver", gameOver);
-		score_txt.text = "0";
-		score = 0;	
-		score_txt.fadeIn(Luxe.screen.mid, 1);
-		volume = 0;
+		_started = false;
+		_ev.listen("addPickup", addPickup);
+		_ev.listen("addObstacle", addObstacle);
+		_ev.listen("gameOver", gameOver);
+		_score_txt.text = "0";
+		_score = 0;	
+		_score_txt.fadeIn(Luxe.screen.mid, 1);
+		_volume = 0;
 	}
 
 	override function onenter<T>( data:T ) {
-		cube.animSmaller(0);
-		cast(cube.get("rotation"), RotatingEntity).setNewVelocity(-40);
-		Actuate.tween(cube.pos, 0.25, {x:Luxe.mouse.x, y:Luxe.mouse.y})
+		_square.animSmaller(0);
+		cast(_square.get("rotation"), RotatingEntity).setNewVelocity( -40 );
+		Actuate.tween(_square.pos, 0.25, {x:Luxe.mouse.x, y:Luxe.mouse.y})
 		.onComplete(begin);
 	}
 
 	override function onleave<T>( data:T ) {
 		Luxe.audio.stop("music");
-		while(obstacles.length > 0)
-		obstacles.pop().destroy();
-		while(bonuses.length > 0)
-		bonuses.pop().destroy();
-		ev.clear();
-		score_txt.fadeOut();
+		while(_obstacles.length > 0)
+		_obstacles.pop().destroy();
+		while(_bonuses.length > 0)
+		_bonuses.pop().destroy();
+		_ev.clear();
+		_score_txt.fadeOut();
 
 	}
 
 	private function hasCollidedToCube(spr:Sprite):Bool {
-		return Hitbox.testCollisionBetweenHitboxes(cast(cube.components.get("hitbox"), Hitbox), cast(spr.components.get("hitbox"), Hitbox));
+		return Hitbox.testCollisionBetweenHitboxes(cast(_square.components.get("hitbox"), Hitbox), cast(spr.components.get("hitbox"), Hitbox));
 	}
 
 	private function addPickup<T>( data:T ) {
-		bonuses.push( new Pickup( ));
-		ev.schedule(8 + Maths.random_float(0, 8), "addPickup");
+		_bonuses.push( new Pickup( ));
+		_ev.schedule(8 + Maths.random_float(0, 8), "addPickup");
 	}
 
 	private function addObstacle<T>( data:T ) {
-		obstacles.push( new Obstacle( ));
-		ev.schedule(5 + Maths.random_float(0, 2), "addObstacle");
+		_obstacles.push( new Obstacle( ));
+		_ev.schedule(5 + Maths.random_float(0, 2), "addObstacle");
 	}
 
 	private function fadeInMusic( ) {
@@ -114,14 +114,14 @@ class GameState extends luxe.State {
 	}
 
 	private function begin() {
-		started = true;
+		_started = true;
 		for( i in 0 ... 5 ) {
 			var obs:Obstacle = new Obstacle();
-			obstacles.push(obs);
+			_obstacles.push(obs);
 		}
 		for( i in 0 ... 5 ) {
 			var obs:Pickup = new Pickup();
-			bonuses.push(obs);
+			_bonuses.push(obs);
 		}
 		addObstacle( null );
 		addPickup( null );
@@ -130,28 +130,28 @@ class GameState extends luxe.State {
 	}
 
 	override function update( delta:Float) {
-		if( !started) return;
-		score += delta;
-		score_txt.text = Std.string(Std.int(score));
+		if( !_started) return;
+		_score += delta;
+		_score_txt.text = Std.string(Std.int(_score));
 
-		if(Lambda.exists(obstacles, hasCollidedToCube)) {
-			ev.queue("gameOver");			
+		if(Lambda.exists(_obstacles, hasCollidedToCube)) {
+			_ev.queue("gameOver");			
 		}
 
-		Lambda.map( Lambda.filter(bonuses, hasCollidedToCube), function( i ) {
+		Lambda.map( Lambda.filter(_bonuses, hasCollidedToCube), function( i ) {
 			cast(i.components.get("move"), MovingEntity).replace( );
-			score += 10;
+			_score += 10;
 			Luxe.audio.play('bleep');
 			if(Maths.random_int(0, 50) == 0)
-				eManager.start("slowmotion");
+			_eManager.start("slowmotion");
 			});
 
-		ev.process();
+		_ev.process();
 	}
 
 	private function gameOver<T>( data:T ) {
 		Luxe.audio.play('hit');
-		started = false;
+		_started = false;
 
 		var save = new LocalSave();
 		// TODO : make the save more clear here. Process more conditions on the LocalSave
@@ -159,14 +159,14 @@ class GameState extends luxe.State {
 			var existingScore:String = save.loadData(highscoreKey);
 			var scoreFromFile:Float = 0;
 			scoreFromFile = Std.parseFloat(existingScore);
-			if(Math.isNaN(scoreFromFile) || scoreFromFile < this.score) {
-				trace("New highscore : " + this.score);
-				save.saveData(highscoreKey, Std.string(this.score));
+			if(Math.isNaN(scoreFromFile) || scoreFromFile < _score) {
+				trace("New highscore : " + _score);
+				save.saveData(highscoreKey, Std.string(_score));
 			}
 
 		}
 
-		eManager.endAll();
+		_eManager.endAll();
 
 		fadeOutMusic();
 
@@ -176,17 +176,17 @@ class GameState extends luxe.State {
 	}
 
 	override function ontouchmove( event:TouchEvent) {
-		cube.pos = event.pos;
+		_square.pos = event.pos;
 	}
 
 	override function onmousemove( event:MouseEvent ) {
-		if( !started ) return;
-		cube.pos = event.pos;
+		if( !_started ) return;
+		_square.pos = event.pos;
 
 	}
 
 	override function onkeydown( event:KeyEvent) {
-		if( event.keycode == Key.escape && this.started)
+		if( event.keycode == Key.escape && _started)
 		gameOver(null);
 	}
 }
